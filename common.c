7ab775c6be56a57075c13242f976326616f26d5b
Gaurav Dugar <gaurav.dugar1506@gmail.com>
2014-11-18 00:01:47 -0500

    adding timeout condition

--- common.c
+++ common.c
@@ -31,6 +31,32 @@ int msg_send(int sockfd, char *ip, int port, char *msg, int flag)
     return n;
 }
 
-struct msg_rec* msg_recv(int sockfd)
+struct msg_rec* msg_recv(int sockfd, int timeout)
 {
+    struct msg_rec *recv;
+    static struct timeval selectTime;
+    int n = 0;
+    fd_set allset;
+    
+    recv = malloc(sizeof(struct msg_rec));
+    selectTime.tv_sec = 3;
+    selectTime.tv_usec = 0;
+    
+    bzero(recv,sizeof(recv));
+    
+    FD_ZERO(&allset);
+    FD_SET(sockfd, &allset);
+    select(sockfd+1, &allset, NULL, NULL, ((timeout) ? (&selectTime) : NULL));
+    if(FD_ISSET(sockfd, &allset)) {
+        n = read(sockfd,recv,sizeof(recv));
+        if (n < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+        printf("Here is the message: %s\n",recv->msg);
+        return recv;
+    } else {
+        printdebuginfo("Timeout occured in receive message..!!\n");
+        return NULL;
+    }
 }
