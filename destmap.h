74fc849c2c3f9e34f1cd608dbab7db217fdae38d
Ankur <ankurmit2006@gmail.com>
2014-11-19 20:07:02 -0500

    Adding structure for destination map

---
+++ destmap.h
@@ -0,0 +1,36 @@
+
+#ifndef _DEST_MAP_H
+#define _DEST_MAP_H
+
+#include "odr.h"
+struct data_wrapper
+{
+  struct odr_hdr odr_hdr;
+  char *data;
+  struct data_wrapper *next;
+};
+
+struct dest_map
+{
+    unsigned long destip;
+    char dest_mac[6];
+    int interface;
+    uint32_t hop;
+    long timestamp; //in millis
+    struct data_wrapper *data; //NULL if no data
+    int resolved;
+    struct dest_map *next;
+}
+static struct portentry *hdestmap = NULL, *tdestmap = NULL;
+
+//Will check sourceip and port no from odr_hdr, if they are same, just replace data, else insert new node
+void insert_data_dest_table(struct odr_hdr odr_hdr, char *data, unsigned long destip);
+
+//if entry is stale, mark resolved as 0 and return NULL, else return node
+// Also if map doesn't contain this destip, crate new node and add it and return NULL
+struct dest_map get_dest_entry(unsigned long destip, long staleness);
+
+//if stale or new hop less than this hop, update entry, if destip not found, add the entry. if entry is updated or added, return this entry else return null
+struct dest_mac update_dest_map(unsigned long destip, char *dest_mac, int interface, uint32_t hop, unsigned long staleness);
+
+#endif
