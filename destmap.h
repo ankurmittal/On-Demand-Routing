eed47db96488f491741783a8f96cce2cb651b6fa
Ankur <ankurmit2006@gmail.com>
2014-11-19 21:40:33 -0500

    Fixing destmap.h

--- destmap.h
+++ destmap.h
@@ -13,7 +13,7 @@ struct data_wrapper
 struct dest_map
 {
     unsigned long destip;
-    char next_hop_mac[6];
+    char dest_mac[6];
     int interface;
     uint32_t hop;
     long timestamp; //in millis
@@ -21,17 +21,17 @@ struct dest_map
     int resolved;
     struct dest_map *next;
 }
-static struct dest_map *hdestmap = NULL, *tdestmap = NULL;
+static struct portentry *hdestmap = NULL, *tdestmap = NULL;
 
 //Will check sourceip and port no from odr_hdr, if they are same, just replace data, else insert new node
-void insert_data_dest_table(struct data_wrapper data_wrapper, unsigned long destip);
+void insert_data_dest_table(struct data_wrapper *data_wrapper, unsigned long destip);
 
 // if entry is stale, mark resolved as 0 and return NULL, else return node
 // Also if map doesn't contain this destip, crate new node and add it and return NULL
 struct dest_map get_dest_entry(unsigned long destip, long staleness);
 
 //if stale or new hop less than this hop, update entry, if destip not found, add the entry. if entry is updated or added, return this entry else return null
-struct dest_mac update_dest_map(unsigned long destip, char *next_hop_mac, int interface, uint32_t hop, unsigned long staleness);
+struct dest_mac update_dest_map(unsigned long destip, char *dest_mac, int interface, uint32_t hop, unsigned long staleness);
 
 //Return first data_wrapper node
 struct data_wrapper get_data_from_queue(unsigned long destip);
