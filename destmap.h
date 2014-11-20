557db01aa76bb48a0012c6ea2f5d8cc41bcf5201
Ankur <ankurmit2006@gmail.com>
2014-11-19 20:08:46 -0500

    Adding function to dest_map

--- destmap.h
+++ destmap.h
@@ -26,11 +26,14 @@ static struct portentry *hdestmap = NULL, *tdestmap = NULL;
 //Will check sourceip and port no from odr_hdr, if they are same, just replace data, else insert new node
 void insert_data_dest_table(struct odr_hdr odr_hdr, char *data, unsigned long destip);
 
-//if entry is stale, mark resolved as 0 and return NULL, else return node
+// if entry is stale, mark resolved as 0 and return NULL, else return node
 // Also if map doesn't contain this destip, crate new node and add it and return NULL
 struct dest_map get_dest_entry(unsigned long destip, long staleness);
 
 //if stale or new hop less than this hop, update entry, if destip not found, add the entry. if entry is updated or added, return this entry else return null
 struct dest_mac update_dest_map(unsigned long destip, char *dest_mac, int interface, uint32_t hop, unsigned long staleness);
+
+//Return first data_wrapper node
+struct data_wrapper get_data_from_queue(unsigned long destip);
 
 #endif
